83b7aca0fda12ff6252f97e220ccec00cc5374d9
Egorfedko <93041614+Egorfedko@users.noreply.github.com>
2026-05-13 14:45:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,46 @@ function togglePDF() {
     let block = document.getElementById("pdfBlock");
     block.classList.toggle("active");
 }
+const hotspots = document.querySelectorAll('.hotspot');
 
+const modal = document.getElementById('modal');
+
+const modalTitle = document.getElementById('modalTitle');
+
+const modalText = document.getElementById('modalText');
+
+const closeModal = document.getElementById('closeModal');
+
+hotspots.forEach(hotspot => {
+
+    hotspot.addEventListener('click', () => {
+
+        const title = hotspot.dataset.title;
+
+        const text = hotspot.dataset.text;
+
+        modalTitle.textContent = title;
+
+        modalText.textContent = text;
+
+        modal.style.display = 'flex';
+
+    });
+
+});
+
+closeModal.addEventListener('click', () => {
+
+    modal.style.display = 'none';
+
+});
+
+window.addEventListener('click', (e) => {
+
+    if(e.target === modal) {
+
+        modal.style.display = 'none';
+
+    }
+
+});
